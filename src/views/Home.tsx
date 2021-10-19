877958e2cd5ced3b934c86194b2dfabb9121a156
Charlie Winnard <ctwinnard@gmail.com>
2021-10-19 15:52:19 -0600

    pet cards

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,24 @@
+import { Card } from '@mui/material';
+
 import user from '../data/user.json';
+import sortedObservations from '../data/sortedObservations.json';
+import '../styles/Home.css';
 
 const Home = () => {
     return (
       <div>
         <div>
           Hello {user.name}
+        </div>
+        <div className="observations-container">
+          {sortedObservations.map((observation) => {
+              return (
+                  <Card className="observation-card">
+                      <img className="pet-avatar" src={observation.picture} />
+                      Hello observation
+                  </Card>
+              )
+          })}
         </div>
       </div>
     );
