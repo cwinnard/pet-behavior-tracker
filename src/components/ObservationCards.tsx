f7824e06f2a8545dc5fb740971f610424c669ac1
Charlie Winnard <ctwinnard@gmail.com>
2021-10-24 12:27:23 -0600

    add time to obs cards

--- src/components/ObservationCards.tsx
+++ src/components/ObservationCards.tsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 import '../styles/ObservationCards.css';
 
-const ObservationCard = ({ id, picture = null, name }) => {
+const ObservationCard = ({ id, picture = null, name, date }) => {
     return (
         <Card className="observation-card">
             <Link to={`/dog/${id}`} style={{ textDecoration: 'none' }}>
             <div className="pet-info">
                 {picture && <img className="pet-avatar" src={picture} />}
                 {name}
+                {date}
             </div>
             </Link>
         </Card>
@@ -25,6 +26,7 @@ const ObservationCard = ({ id, picture = null, name }) => {
                         id={observation.id}
                         picture={observation.picture}
                         name={observation.observation}
+                        date={observation.timestamp}
                     />
                 )
             })}
