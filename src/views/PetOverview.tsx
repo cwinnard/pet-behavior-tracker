77e53bd5eac75708835a4cb88b07baa736d858d2
Charlie Winnard <ctwinnard@gmail.com>
2021-10-24 11:21:40 -0600

    add chips to profile

--- src/views/PetOverview.tsx
+++ src/views/PetOverview.tsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router';
+import { Chip } from '@mui/material';
 
+import '../styles/PetOverview.css';
 import dogs from '../data/dogs.json';
 
 
@@ -8,10 +10,15 @@ const PetOverview = () => {
   const { id } = useParams() as any;
   const dog = dogs.find(dog => dog.id === id) as any;
   return (
-    <div>
-      <div>
-        Hello {dog.name} id {id}
+    <div className="profile-container">
+      Hello {dog.name} id {id}
+      <img className="profile-picture" src={dog.picture} />
+      <div className="chip-container">
+         <Chip label="Happy" /> 
+         <Chip label="Energetic" /> 
+         <Chip label="Timid" /> 
       </div>
+      
     </div>
   );
 }
