4823e82bc2e21c3d570cba71a23577e82dbe2815
Charlie Winnard <ctwinnard@gmail.com>
2021-11-14 11:26:58 -0700

    building observation input

--- src/components/ObservationDrawer.tsx
+++ src/components/ObservationDrawer.tsx
@@ -1,4 +1,4 @@
-import { Drawer, Paper } from '@mui/material';
+import { Drawer, MenuItem, Paper, TextField } from '@mui/material';
 import { useState } from 'react';
 
 import { mutate } from '../api/clients/HasuraClient';
@@ -6,11 +6,18 @@ import newObservationMutation from '../api/graphql/newObservationMutation';
 
 import '../styles/ObservationDrawer.css';
 
+// TODO own file
+const categories = {
+    'Behavior': ['Happy', 'Spastic'],
+    'Appetite': ['Low', 'Healthy', 'Insatiable']
+};
+
 
 const ObservationDrawer = ({ open, handleClose }) => {
     const [dogId, setDogId] = useState(1);
-    const [behavior, setBehavior] = useState('test');
-    const [category, setCategory] = useState('test');
+    const [category, setCategory] = useState('');
+    const [behavior, setBehavior] = useState('');
+    const [observationOptions, setObservationOptions] = useState([]);
 
     const handleSubmit = () => {
         mutate(newObservationMutation, {
@@ -20,6 +27,11 @@ const ObservationDrawer = ({ open, handleClose }) => {
         }).then((response: any) => {
           console.log('ok')
         });
+    }
+
+    const handleCategoryChange = (value) => {
+        setCategory(value);
+        setObservationOptions(categories[value])
     }
 
     return (
@@ -32,6 +44,28 @@ const ObservationDrawer = ({ open, handleClose }) => {
             <div className="drawer-top" />
             <Paper className="input-container">
                 <p>Hello modal</p>
+                {/* <TextField onChange={event => setDogId(event?.target.value)}>
+                </TextField> */}
+                <TextField
+                  select
+                  value={category}
+                  onChange={event => handleCategoryChange(event.target.value)}
+                >
+                    {Object.keys(categories).map(category => (
+                        <MenuItem value={category}>
+                            {category}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                {category && (
+                    <div className="options-container">
+                        {observationOptions.map(option => (
+                            <div>
+                                hello {option}
+                            </div>
+                        ))}
+                    </div>
+                )}
                 <button onClick={() => handleSubmit()}>
                     Submit
                 </button>
